1545f5fafc6c838eb79a8ed530e361fae0192f62
JoseChavez98 <jose.chavez@utec.edu.pe>
2019-04-15 21:21:21 -0500

    fixing local

---
+++ lab2.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+void pointlessFunction(){
+    printf("El costo de llamar a una función mínima en C /C++: ")
+}
+
+int main(){
+
+    struct timeval start;
+    struct timeval finish;
+    long compTime;
+    double Time;
+
+    gettimeofday(&start, 0);
+
+    return 0;
+}
